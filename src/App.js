ded89ca1a6e9bce32f22b8601f0b7c1b7a55daf7
CathGlover <117387100+CathGlover@users.noreply.github.com>
2023-04-24 08:06:02 +0100

    update github link

--- src/App.js
+++ src/App.js
@@ -7,5 +7,6 @@ export default function App() {
       <h1>Weather App</h1>
       <WeatherSearchEngine />
     </div>
+    <p>Open Source Code, coded by Catherine Glover, code on <a href = "https://github.com/CathGlover/weather-react-shecodes-app">GitHub</a></p>
   );
 }
