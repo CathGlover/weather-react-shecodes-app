91dc242e6053eb289bce1be65a8ce458ec68ecc5
CathGlover <117387100+CathGlover@users.noreply.github.com>
2023-05-05 11:14:28 +0100

    correctly defined forecast data

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -8,7 +8,7 @@ export default function WeatherForecast(event) {
   let url = `https://api.shecodes.io/weather/v1/forecast?query=${event.data.data.cities}&key=edf069311acf2bebo10f4bbbc53249t3&units=metric`;
   axios.get(url).then(handleResponse);
 
-  const [forecast, setForecast] = useState({});
+  const [forecast, setForecast] = useState(null);
   const [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
@@ -17,20 +17,22 @@ export default function WeatherForecast(event) {
     console.log(response);
   }
 
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        {forecast.map(function (dailyForecast, index) {
-          if (index < 5 || index > 0) {
-            return (
-              <div className="col" key={index}>
-                <WeatherForecastDay data={dailyForecast} />
-              </div>
-            );
-          }
-        })}
-        ;
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5 || index > 0) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
+          ;
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -3,18 +3,21 @@ import FormattedDate from "./FormattedDate";
 import "./WeatherForecast.css";
 import WeatherIcon from "./WeatherIcon";
 export default function WeatherForecastDay(event) {
-  return (<div className = "WeatherForecastDay">
-  <FormattedDate date={event.date} />
-    <span>
-      <WeatherIcon icon={event.icon} size={52} />
-    </span>
-    <span className="forecastTemperature">
-      <span className="forecastTemperatureMax">
-        {event.temperature.maximum}
+  console.log(event);
+  return (
+    <div className="WeatherForecastDay">
+      <FormattedDate date={event.data.time} />
+      <span>
+        <WeatherIcon icon={event.data.icon} size={52} />
       </span>
-      <span className="forecastTemperatureMin">
-        {event.temperature.minimimum}
+      <span className="forecastTemperature">
+        <span className="forecastTemperatureMax">
+          {event.data.temperature.maximum}
+        </span>
+        <span className="forecastTemperatureMin">
+          {event.data.temperature.minimum}
+        </span>
       </span>
-    </span>
-  </div>);
+    </div>
+  );
 }
